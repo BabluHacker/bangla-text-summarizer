237d339d13ec98e05bd2efd8f910aeccfbbed1f3
Mehedi Hasan <mehedi1055@gmail.com>
2019-08-21 11:13:09 +0600

    init

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, request, redirect, send_from_directory, url_for, jsonify
+from flask import Flask, request, jsonify
 from mainFile import start
 app = Flask(__name__)
 @app.route('/summarize', methods=['POST'])
 def summarize():
     text = request.form['text']
-    ret = start(text)
-
-    return jsonify(ret), 200
+    try:
+        ret = start(text)
+        return jsonify(ret), 200
+    except Exception as e:
+        ret_text = {}
+        ret_text['status'] = 0
+        return jsonify(ret_text), 400
 if __name__ == '__main__':
     app.run()
