f35a0821c896c4da4ff07b19429a820c5ccb49bc
root <root@ip-172-31-21-154.ap-south-1.compute.internal>
2020-01-31 19:57:30 +0600

    init

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from mainFile import start
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
-cors = CORS(app, resources={r"/*": {"origins": "https://summarize.technocrats.io"}})
+cors = CORS(app, resources={r"/*": {"origins": "http://summarize.technocrats.io"}})
 @app.route('/', methods=['POST'])
 def summarize():
     text = request.form['text']
-    try:
-        ret = start(text)
-        ret['status'] = 1
-        return jsonify(ret), 200
-    except Exception as e:
-        ret_text = {}
-        ret_text['status'] = 0
-        return jsonify(ret_text), 400
+#    try:
+    ret = start(text)
+    ret['status'] = 1
+    return jsonify(ret), 200
+#    except Exception as e:
+#        ret_text = {}
+#        ret_text['status'] = 0
+#        return jsonify(ret_text), 400
 if __name__ == '__main__':
     app.run()
--- run_bn_summarize.sh
+++ run_bn_summarize.sh
@@ -5,8 +5,8 @@ echo "starting summarizer...."
 
 source venv/bin/activate
 
-PATH=$PATH:/home/ubuntu/technocratsProjects/bangla-text-summarizer/venv/bin/python:/home/ubuntu/technocratsProjects/bangla-text-summarizer/ven$
-export PATH
+#PATH=$PATH:/home/ubuntu/technocratsProjects/bangla-text-summarizer/venv/bin/python:/home/ubuntu/technocratsProjects/bangla-text-summarizer/ven$
+#export PATH
 #gunicorn -w 14 -b 127.0.0.1:3000 app:app --timeout 300 --log-file logs --daemon
 gunicorn -b 127.0.0.1:5000 app:app --timeout 300
 echo "stopping summarizer"
